2e5675516abafd5d97563e63d5816dfba040b0a1
Kate Follett <git@klgf.dev>
2021-06-09 11:36:55 -0400

    created header title component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import './App.css';
+import Header from "./components/header"
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Header />
   );
-}
+};
 
 export default App;
