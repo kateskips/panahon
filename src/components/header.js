20d4c1d2e2d84a72200e2a9e0a1e3ebfd5ffca45
Kate Follett <git@klgf.dev>
2021-06-11 15:50:13 -0400

    added wrapper and container

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,20 @@ import "../App.css";
 
 const Header = () => {
     return (
-        <div className="header-title">
-            <h1>Panahon</h1>  
+        <div className="wrapper">
+            <div className="header-title">
+                <h1>Panahon</h1>
+                <div className="nes-container is-rounded" >
+                    <p>8-Bit Weather App</p>
+                </div>
+                <div className="main">
+                <row class="nes-btn">
+                <button type="button" class="nes-btn is-primary">about</button>
+                <button type="button" class="nes-btn is-error">contact</button>
+                <button type="button" class="nes-btn is-success">source</button>
+                </row>
+            </div>
+            </div>
         </div>
     );
 };
