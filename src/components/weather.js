42886df8de90adcd136517b4b9b8b6f6ebb89075
Kate Follett <git@klgf.dev>
2021-06-18 14:08:43 -0400

    renamed and changed components

---
+++ src/components/weather.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "../App.css";
+
+const Weather = () => {
+    return (
+        <div className="wrapper">
+            <div className="header-title">
+                <h1>Panahon</h1>
+                <div className="nes-container is-rounded" >
+                    <p>8-Bit Weather App</p>
+                </div>
+                <div className="nes-container is-rounded">
+                    <p>Weather</p>
+                </div>
+                <div className="nes-container is-rounded">
+                    <div className="message-list">
+                        <div className="message -left">
+                            <div className="nes-balloon from-left">
+                                <p>Hello NES.css</p>
+                            </div>
+                        </div>
+                        <div className="cat-icon">
+                        <img src="https://img.icons8.com/material-two-tone/35/000000/pixel-cat.png" alt="cat"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Weather;
