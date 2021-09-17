5d163cb3f199ef9ca81233426d38662a5ed973c0
Kate Follett <git@klgf.dev>
2021-09-17 13:43:29 -0400

    converted to a class function

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import './App.css';
 import Weather from "./components/weather";
 
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+const API_key = "23722037aaa01481ac18d22d223d2373"
 
-//*const API_key = "23722037aaa01481ac18d22d223d2373"
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {}
+  }
 
-
-function App() {
-  return (
-    <Weather />
-  );
-};
+  
+  render(){
+    return(<Weather />);
+  }
+}
 
 export default App;
