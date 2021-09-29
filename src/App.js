bf727444bcd6feccaee76ab49f0e6bb785b669d7
Kate Follett <git@klgf.dev>
2021-09-29 17:19:30 -0400

    formatted

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import './App.css';
 import Weather from "./components/weather";
 
+
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 const API_key = "f74159910fbfeca96450bd09405e3b33"
 
@@ -36,10 +37,12 @@ class App extends React.Component {
     })
   }
   render(){
-    return(<Weather 
+    return(
+    <Weather 
       city={this.state.city}
       temp={this.state.temp}
-      description={this.state.description} />);
+      description={this.state.description} />
+    );
   }
 }
 
