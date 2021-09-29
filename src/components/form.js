079f31fad6798841ff1fdfc26407279ebb2c6904
Kate Follett <git@klgf.dev>
2021-09-29 17:19:01 -0400

    developed a form for weather

--- src/components/form.js
+++ src/components/form.js
@@ -1,12 +1,14 @@
-import { Form } from "semantic-ui-react";
-import React from react;
+import React from 'react';
 import "../App.css";
 
-const Form = () => {
+const Form = props => {
     return (
-        
+        <div className="nes-field is-inline">
+            <label for="error_field"></label>
+            <input type="text" id="name_field" className="nes-btn" placeholder="city name" className="form-control" name="city" autoComplete="off" />
+            <a class="nes-btn is-warning" href="#">Get Weather</a>    
+    </div>
     );
 };
-
-
+   
 export default Form;
